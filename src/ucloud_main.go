7dc3f9147abbb71e15408cdd2078e20f8cd80949
yanghengli <thehenry@163.com>
2022-01-04 11:37:46 +0800

    test 2

--- src/ucloud_main.go
+++ src/ucloud_main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	appDesc := "================================== UCloud API 测试开始 =================================="
+	appDesc := "============== UCloud API 测试开始 =============="
 	fmt.Println(appDesc)
 	fmt.Println("测试结束.")
 }
