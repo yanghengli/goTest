690c1573155c36804460afef81823650abdfa1e9
yanghengli <thehenry@163.com>
2022-01-04 10:56:02 +0800

    test

---
+++ src/ucloud_main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	appDesc := "================================== UCloud API 测试开始 =================================="
+	fmt.Println(appDesc)
+	fmt.Println("测试结束.")
+}
